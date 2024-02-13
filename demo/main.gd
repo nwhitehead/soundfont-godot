ed82d41ec10c96e78b6443fd989c55baa9c78ddb
Nathan Whitehead <nwhitehe@gmail.com>
2024-02-13 14:20:19 -0700

    Try Fluid GM kit.

--- demo/main.gd
+++ demo/main.gd
@@ -4,19 +4,22 @@ var sf: SoundFont = null
 var sfg: SoundFontGenerator = null
 var playback: AudioStreamPlayback = null # Actual playback stream, assigned in _ready().
 var time: float = 0.0
+# 42 # cello
+# 58 # tuba
+var preset: int = 58
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sf = SoundFont.new()
 	# var file = FileAccess.open('res://example_broken.sf2', FileAccess.READ)
-	var file = FileAccess.open('res://example.sf2', FileAccess.READ)
+	var file = FileAccess.open('res://FluidR3_GM.sf2', FileAccess.READ)
 	var data = file.get_buffer(file.get_length())
 	sf.set_data(data)
 	sfg = $SoundFontGenerator
 	playback = sfg.get_node("AudioStreamPlayer").get_stream_playback();
 	sfg.set_soundfont(sf)
-	print(sfg.get_presetname(0))
-	sfg.note_on(0, 44 - 24, 1.0)
+	print(sfg.get_presetname(preset))
+	# sfg.note_on(0, 44 - 24, 1.0)
 	var out = sfg.render(10)
 	print(out)
 
@@ -39,9 +42,9 @@ func _input(event):
 	}
 	for note in notes:
 		if event.is_action_pressed(note):
-			sfg.note_on(0, 44 + notes[note], 1.0)
+			sfg.note_on(preset, 44 + notes[note], 1.0)
 		if event.is_action_released(note):
-			sfg.note_off(0, 44 + notes[note])
+			sfg.note_off(preset, 44 + notes[note])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
