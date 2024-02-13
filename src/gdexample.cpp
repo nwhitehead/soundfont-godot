1ae0f891060ade4bdf6e8b14e21cf1a354a1d25f
Nathan Whitehead <nwhitehe@gmail.com>
2024-02-12 17:15:51 -0700

    Add audiostream resource to demo.

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -6,7 +6,10 @@ using namespace godot;
 void SoundFont::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_mix_rate", "hz"), &SoundFont::set_mix_rate);
 	ClassDB::bind_method(D_METHOD("get_mix_rate"), &SoundFont::get_mix_rate);
+    ClassDB::bind_method(D_METHOD("set_stream", "stream"), &SoundFont::set_stream);
+    ClassDB::bind_method(D_METHOD("get_stream"), &SoundFont::get_stream);
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "mix_rate", PROPERTY_HINT_RANGE, "20,192000,1,suffix:Hz"), "set_mix_rate", "get_mix_rate");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "stream", PROPERTY_HINT_RESOURCE_TYPE, "AudioStream"), "set_stream", "get_stream");
 }
 
 SoundFont::SoundFont() {
@@ -25,6 +28,14 @@ void SoundFont::set_mix_rate(float p_mix_rate) {
 
 float SoundFont::get_mix_rate() const {
     return mix_rate;    
+}
+
+void SoundFont::set_stream(Ref<AudioStream> p_stream) {
+    stream = p_stream;
+}
+
+Ref<AudioStream> SoundFont::get_stream() const {
+    return stream;
 }
 
 void SoundFont::_process(double delta) {
